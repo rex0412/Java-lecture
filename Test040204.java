5ed10a53dee51091fda4e798989dbe0dec7781ba
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-05-24 15:12:26 +0900

    이중for문 방정식 구하기

---
+++ Test040204.java
@@ -0,0 +1,20 @@
+package submit;
+
+public class Test040204 {
+	
+	public static void main(String[] args) {
+		
+		  for (int i = 0; i <= 10; i++) {
+			   for (int j = 0; j <= 10; j++) {
+
+			    if (((4 * i) + (5 * j)) == 60) {
+			     
+			    	System.out.println("(" + i + "," + j + ")");
+	      }
+
+         }
+      }
+
+   }
+
+}
