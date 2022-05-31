a75ddd780a842de88b87022e1eb8e043349d7567
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-05-31 14:27:07 +0900

    Add files via upload

---
+++ ShopService.java
@@ -0,0 +1,12 @@
+package day07;
+
+public class ShopService {
+	private static ShopService singleton = new ShopService();
+	
+	private ShopService() {}
+
+	public static ShopService getInstance() {
+		return singleton;
+	}
+
+}
